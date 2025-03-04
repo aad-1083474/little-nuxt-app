be995b993fd92732e8aa9d68e8dad922b7bc1ab3
Aad <1083474@hr.nl>
2025-03-04 14:57:28 +0100

    basic userinput msg

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+    {{ userInput }}
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup> 
+import { ref } from 'vue'
+  const userInput = ref('This is an XSS attack')
+</script>
